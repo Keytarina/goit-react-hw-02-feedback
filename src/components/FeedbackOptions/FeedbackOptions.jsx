6426bddbf86739acb43f4b83a90b51b0ec0eb64c
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-07-26 23:24:39 +0300

    Create feedback-app

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div className={css.btn_wrapper}>
+    {options.map((option) => {
+      return (
+        <button 
+        className={css.btn} 
+        type="button" 
+        key={option}
+        name={option}
+        onClick={onLeaveFeedback}
+      >{option}</button>
+      )
+    })}
+    </div>
+  )	
+}
+
+FeedbackOptions.propTypes = {
+	onLeaveFeedback: PropTypes.func.isRequired,
+};
